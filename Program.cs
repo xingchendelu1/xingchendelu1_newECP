3ab34c3cda1a036349e1686ad0f4dcd0d444ecef
xingc <xingc@DESKTOP_JJ1>
2023-03-29 18:56:14 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace xingchendelu1_newECP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this is my new flower to u!");
+        }
+    }
+}
